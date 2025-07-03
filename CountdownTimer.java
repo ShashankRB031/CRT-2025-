a1f0564b656a283ac7100f970f7e18996947209f
Shashank R B <rbshashank1@gmail.com>
2025-07-03 07:35:33 +0530

    Updated files

--- CountdownTimer.java
+++ CountdownTimer.java
@@ -1,3 +1,9 @@
+/*4] Mr.Kashyap is working as software enggineer finalising sprints(short meetings) 
+for banking project, the patchup work of the software has a timing deadline 
+of 5 second s an allaram is setup using a counter to display 
+the reverse counting of the timer generate a code to help Mr.Kashyap */
+
+
 public class CountdownTimer {
     public static void main(String[] args) {
         int countdownTime = 5; 
@@ -9,7 +15,7 @@ public class CountdownTimer {
             System.out.println("Time left: " + i + " seconds");
 
             try {
-                Thread.sleep(1000); 
+                Thread.sleep(10000); 
             } catch (InterruptedException e) {
                 System.out.println("Timer interrupted!");
             }
